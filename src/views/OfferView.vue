75a7dd4dcdac3e045f6f972b52d34cde2c0d7439
Chaouch Nizar <chaouchnizar1@gmail.com>
2024-03-18 04:04:47 +0100

    add navbar

--- src/views/OfferView.vue
+++ src/views/OfferView.vue
@@ -1,8 +1,14 @@
 <script>
-export default {};
+import NavBar from "@/components/public/NavBar.vue";
+export default {
+    components: {
+        NavBar
+    }
+};
 </script>
 
 <template>
+    <NavBar />
   <div></div>
 </template>
 <style lang=""></style>
