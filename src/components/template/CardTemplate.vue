dbc00aef53f59f735feb68656317c5d63c166f23
Chaouch Nizar <chaouchnizar1@gmail.com>
2024-03-18 03:19:09 +0100

    add template

---
+++ src/components/template/CardTemplate.vue
@@ -0,0 +1,127 @@
+<script>
+export default {
+  data: () => ({
+    search: "",
+    games: [
+      {
+        img: "https://cdn.vuetifyjs.com/docs/images/graphics/games/4.png",
+        title: "Template 1",
+      },
+      {
+        img: "https://cdn.vuetifyjs.com/docs/images/graphics/games/2.png",
+        title: "Template 2",
+      },
+      {
+        img: "https://cdn.vuetifyjs.com/docs/images/graphics/games/3.png",
+        title: "Template 3",
+      },
+      {
+        img: "https://cdn.vuetifyjs.com/docs/images/graphics/games/5.png",
+        title: "Template 4",
+      },
+      {
+        img: "https://cdn.vuetifyjs.com/docs/images/graphics/games/6.png",
+        title: "Template 5",
+      },
+      {
+        img: "https://cdn.vuetifyjs.com/docs/images/graphics/games/7.png",
+        title: "Template 6",
+      },
+      {
+        img: "https://cdn.vuetifyjs.com/docs/images/graphics/games/1.png",
+        title: "Template 7",
+      },
+      {
+        img: "https://cdn.vuetifyjs.com/docs/images/graphics/games/8.png",
+        title: "Template 8",
+      },
+    ],
+  }),
+};
+</script>
+<template>
+  <v-card>
+    <v-data-iterator :items="games" :items-per-page="6" :search="search">
+      <template v-slot:header>
+        <v-toolbar class="px-2">
+          <v-text-field
+            v-model="search"
+            density="comfortable"
+            placeholder="Search"
+            prepend-inner-icon="mdi-magnify"
+            style="max-width: 300px"
+            variant="solo"
+            clearable
+            hide-details
+          ></v-text-field>
+        </v-toolbar>
+      </template>
+
+      <template v-slot:default="{ items }">
+        <v-container class="pa-2" fluid>
+          <v-row dense>
+            <v-col v-for="item in items" :key="item.title" cols="auto" md="4">
+              <v-hover v-slot="{ isHovering, props }">
+                <v-card class="mb-6" border flat v-bind="props">
+                  <v-img :src="item.raw.img"></v-img>
+
+                  <v-overlay
+                    :model-value="isHovering"
+                    class="align-center justify-center"
+                    scrim
+                    contained
+                  >
+                    <v-btn
+                      variant="flat"
+                      class="text-none d-flex align-center ma-2 rounded-pill"
+                      size="large"
+                      color="#5865f2"
+                      >Modifier</v-btn
+                    >
+                    <v-btn
+                      variant="outlined"
+                      class="text-none d-flex align-center ma-2 ms-6 rounded-pill"
+                      size="large"
+                      color="white"
+                      >Voir</v-btn
+                    >
+                  </v-overlay>
+                </v-card>
+              </v-hover>
+
+              <v-list-item class="mb-2">
+                <div>{{ item.raw.title }}</div>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-container>
+      </template>
+
+      <template v-slot:footer="{ page, pageCount, prevPage, nextPage }">
+        <div class="d-flex align-center justify-center pa-4">
+          <v-btn
+            :disabled="page === 1"
+            density="comfortable"
+            icon="mdi-arrow-left"
+            variant="tonal"
+            rounded
+            @click="prevPage"
+          ></v-btn>
+
+          <div class="mx-2 text-caption">
+            Page {{ page }} sur {{ pageCount }}
+          </div>
+
+          <v-btn
+            :disabled="page >= pageCount"
+            density="comfortable"
+            icon="mdi-arrow-right"
+            variant="tonal"
+            rounded
+            @click="nextPage"
+          ></v-btn>
+        </div>
+      </template>
+    </v-data-iterator>
+  </v-card>
+</template>
--- src/views/TemplateView.vue
+++ src/views/TemplateView.vue
@@ -1,17 +1,18 @@
 <script>
 import NavBar from "@/components/public/NavBar.vue";
+import CardTemplate from "@/components/template/CardTemplate.vue";
 import { mapActions } from "vuex";
 export default {
   name: "template",
   components: {
     NavBar,
+    CardTemplate,
   },
   methods: {
-    ...mapActions(["userAuth", "userAuthr"]),
+    ...mapActions(["userAuth"]),
   },
   mounted() {
     this.userAuth();
-    this.userAuthr();
   },
 };
 </script>
@@ -19,9 +20,10 @@ export default {
   <NavBar />
   <v-container class="mt-16">
     <v-row>
-      <v-col cols="12" sm="12" md="10" xl="8" class="ma-9">
+      <v-col cols="12" sm="12" md="10" xl="8" class="ma-lg-9">
         <h1>Sélectionnez le template que vous aimez</h1>
       </v-col>
     </v-row>
+    <CardTemplate />
   </v-container>
 </template>
