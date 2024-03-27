8865a941ab82adadd6db573b0426166d660c69af
Chaouch Nizar <chaouchnizar1@gmail.com>
2024-03-27 07:02:36 +0100

    *

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -56,51 +56,6 @@ export default {
         ></v-img>
       </v-sheet>
     </v-sheet>
-    <!-- ddd -->
-    <v-sheet
-      class="mx-auto px-16 "
-      min-height="200"
-      max-width="1200"
-      color="transparent"
-    >
-      <h1 class="text-h3 font-weight-black text-center">
-        Créer un <span>portfolio d'exception</span> et trouvez des opportunités
-      </h1>
-      <v-sheet
-        class="mx-auto mt-5  text-h6 text-center"
-        max-width="1000"
-        color="#FFFFFF00"
-      >
-        Bienvenue sur portfolio xception, la platform ultime pour créer un
-        portefeuille professionnel et explorer des possibilités de carrière
-        exceptionnelles, offrant une expérience utilisateur intuitive et
-        conviviale. <br />
-        <v-btn
-          color="success"
-          elevation="10"
-          size="x-large"
-          class="text-none ma-6 btn"
-          rounded="xl"
-          to="signup"
-          >Créer maintenant</v-btn
-        >
-      </v-sheet>
-      <v-sheet
-        class="mx-auto px-16"
-        min-height="auto"
-        color="#FFFFFF00"
-        :elevation="24"
-        rounded
-      >
-        <v-img
-          cover
-          aspect-ratio="4/3"
-          min_height="auto"
-          lazy-src="https://zyro.com/cdn-cgi/image/w=1920,q=85,f=auto/images/portfolioWebsite/shop-template.png"
-          src="https://zyro.com/cdn-cgi/image/w=1920,q=85,f=auto/images/portfolioWebsite/shop-template.png"
-        ></v-img>
-      </v-sheet>
-    </v-sheet>
   </div>
 </template>
 
