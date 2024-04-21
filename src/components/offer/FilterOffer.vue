6349b83379453da61611db925b41c9d9c44dfeba
Chaouch Nizar <chaouchnizar1@gmail.com>
2024-04-22 00:27:57 +0100

    add button filter

--- src/components/offer/FilterOffer.vue
+++ src/components/offer/FilterOffer.vue
@@ -1,22 +1,37 @@
 <script>
 export default {
   data: () => ({
-    sheet: false,
+    sheet: true,
   }),
 };
 </script>
 <template>
   <div class="text-center">
-    <v-btn size="x-large" text="Click Me" @click="sheet = !sheet"></v-btn>
+    <v-btn class="mt-4" size="50" @click="sheet = !sheet">
+      <v-icon size="35">mdi-tune</v-icon></v-btn
+    >
 
     <v-layout>
       <v-navigation-drawer
         v-model="sheet"
+        :width="320"
         location="start"
         elevation="10"
         class="mt-16"
       >
-        <v-icon @click="drawer = !drawer" class="ma-3">mdi-close</v-icon>
+        <v-row class="ma-4">
+          <v-icon size="25" class="mt-1">mdi-tune</v-icon>
+          <p class="font-weight-bold text-h6 ms-2">Filtres</p>
+          <v-btn
+            variant="text"
+            icon="mdi-menu-left"
+            size="30"
+            class="ms-auto"
+            @click="sheet = !sheet"
+          ></v-btn>
+        </v-row>
+        <v-divider></v-divider>
+
         <v-list density="compact">
           <v-list-item title="accueil" to="/"> </v-list-item>
           <v-list-item title="templates" to="template"></v-list-item>
