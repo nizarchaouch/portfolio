817786a5891711e11e9d60f941c5c962ba56fc4b
Chaouch Nizar <chaouchnizar1@gmail.com>
2024-06-02 20:47:38 +0100

    add link dyn

--- src/views/recruteur/ProfilView.vue
+++ src/views/recruteur/ProfilView.vue
@@ -67,16 +67,16 @@ export default {
                     size="68"
                     rounded="0"
                     class="rounded-lg ms-4"
-                    :image="'http://localhost:8000' + this.user.userData.logoPath"
+                    :image="'http://localhost:8000' + userData.logoPath"
                   >
                   </v-avatar>
                 </v-col>
                 <v-col cols="12" md="3">
                   <p class="font-weight-bold text-h6">
-                    {{ this.user.userData.nomEntreprise }}
+                    {{ userData.nomEntreprise }}
                   </p>
                   <span class="text-subtitle-1 text-medium-emphasis">
-                    {{ this.user.userData.secteur }}
+                    {{ userData.secteur }}
                   </span>
                 </v-col>
                 <!-- Suivez-nous sur -->
@@ -87,91 +87,82 @@ export default {
                         Suivez-nous sur:
                       </span>
                     </v-col>
-                    <v-col cols="auto" class="ms-2">
+                    <v-col
+                      cols="auto"
+                      class="ms-2"
+                      v-for="(link, index) in userData.socialLinks"
+                      :key="index"
+                    >
                       <!-- icon facebook -->
                       <v-btn
+                        v-if="link.platform === 'Facebook'"
+                        :href="link.url"
                         size="40"
                         variant="text"
                         class="mx-1"
                         color="indigo"
                       >
-                        <!-- <v-icon size="x-large"> mdi-facebook </v-icon> -->
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          x="0px"
-                          y="0px"
-                          width="41"
-                          height="41"
-                          viewBox="0 0 48 48"
+                        <v-avatar
+                          image="https://img.icons8.com/?size=100&id=yGcWL8copNNQ&format=png&color=000000"
                         >
-                          <linearGradient
-                            id="awSgIinfw5_FS5MLHI~A9a_yGcWL8copNNQ_gr1"
-                            x1="6.228"
-                            x2="42.077"
-                            y1="4.896"
-                            y2="43.432"
-                            gradientUnits="userSpaceOnUse"
-                          >
-                            <stop offset="0" stop-color="#0d61a9"></stop>
-                            <stop offset="1" stop-color="#16528c"></stop>
-                          </linearGradient>
-                          <path
-                            fill="url(#awSgIinfw5_FS5MLHI~A9a_yGcWL8copNNQ_gr1)"
-                            d="M42,40c0,1.105-0.895,2-2,2H8c-1.105,0-2-0.895-2-2V8c0-1.105,0.895-2,2-2h32	c1.105,0,2,0.895,2,2V40z"
-                          ></path>
-                          <path
-                            d="M25,38V27h-4v-6h4v-2.138c0-5.042,2.666-7.818,7.505-7.818c1.995,0,3.077,0.14,3.598,0.208	l0.858,0.111L37,12.224L37,17h-3.635C32.237,17,32,18.378,32,19.535V21h4.723l-0.928,6H32v11H25z"
-                            opacity=".05"
-                          ></path>
-                          <path
-                            d="M25.5,37.5v-11h-4v-5h4v-2.638c0-4.788,2.422-7.318,7.005-7.318c1.971,0,3.03,0.138,3.54,0.204	l0.436,0.057l0.02,0.442V16.5h-3.135c-1.623,0-1.865,1.901-1.865,3.035V21.5h4.64l-0.773,5H31.5v11H25.5z"
-                            opacity=".07"
-                          ></path>
-                          <path
-                            fill="#fff"
-                            d="M33.365,16H36v-3.754c-0.492-0.064-1.531-0.203-3.495-0.203c-4.101,0-6.505,2.08-6.505,6.819V22h-4v4	h4v11h5V26h3.938l0.618-4H31v-2.465C31,17.661,31.612,16,33.365,16z"
-                          ></path>
-                        </svg>
+                        </v-avatar>
                       </v-btn>
                       <!-- btn linekdin -->
-                      <v-btn size="40" variant="text" class="mx-1" color="blue">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          x="0px"
-                          y="0px"
-                          width="41"
-                          height="41"
-                          viewBox="0 0 48 48"
+                      <v-btn
+                        v-if="link.platform === 'LinkedIn'"
+                        :href="link.url"
+                        size="40"
+                        variant="text"
+                        class="mx-1"
+                        color="blue"
+                      >
+                        <v-avatar
+                          image="https://img.icons8.com/?size=100&id=13930&format=png&color=000000"
                         >
-                          <path
-                            fill="#0288D1"
-                            d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"
-                          ></path>
-                          <path
-                            fill="#FFF"
-                            d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"
-                          ></path>
-                        </svg>
+                        </v-avatar>
                       </v-btn>
                       <!-- btn twitter -->
                       <v-btn
+                        v-if="link.platform === 'Twitter'"
+                        :href="link.url"
                         size="40"
                         variant="text"
                         class="mx-1"
                         color="black"
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          x="0px"
-                          y="0px"
-                          width="37"
-                          height="37"
-                          viewBox="0 0 50 50"
+                        <v-avatar
+                          image="https://img.icons8.com/?size=100&id=ClbD5JTFM7FA&format=png&color=000000"
                         >
-                          <path
-                            d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"
-                          ></path>
-                        </svg>
+                        </v-avatar>
+                      </v-btn>
+                      <!-- btn Instagram -->
+                      <v-btn
+                        v-if="link.platform === 'Instagram'"
+                        :href="link.url"
+                        size="40"
+                        variant="text"
+                        class="mx-1"
+                        color="red-darken-3"
+                      >
+                        <v-avatar
+                          image="https://img.icons8.com/?size=100&id=Xy10Jcu1L2Su&format=png&color=000000"
+                        >
+                        </v-avatar>
+                      </v-btn>
+                      <!-- btn Instagram -->
+                      <v-btn
+                        v-if="link.platform === 'Instagram'"
+                        :href="link.url"
+                        size="40"
+                        variant="text"
+                        class="mx-1"
+                        color="black"
+                      >
+                        <v-avatar
+                          size="35"
+                          image="https://img.icons8.com/?size=100&id=2963&format=png&color=000000"
+                        >
+                        </v-avatar>
                       </v-btn>
                     </v-col>
                   </v-row>
